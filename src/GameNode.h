947de88a20f5a87060154b51d7e6ee19bae78ae4
YSY <shaoyuanyu@foxmail.com>
2024-04-10 19:01:30 +0800

    fix: 修复GameNode中的FLT_MIN问题

--- src/GameNode.h
+++ src/GameNode.h
@@ -16,7 +16,7 @@ struct GameNode {
     Turn turn; // 一回合，两手落子
     bool isMaxNode; // 是否为Max节点
     float alpha = FLT_MAX; // 上确界
-    float beta = FLT_MIN; // 下确界
+    float beta = -FLT_MAX; // 下确界
 
     GameNode(): turn(Turn(-1, -1, -1, -1)), isMaxNode(true) {}
     GameNode(Turn turn, bool isMaxNode): turn(turn), isMaxNode(isMaxNode) {}
